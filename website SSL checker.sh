248c382af0244044c816e7e6e8aeb4d5f616091d
george-redsunis <george@redsunis.com>
2017-06-23 16:50:06 -0400

    Used for marketing purposes
    This is a project that works off a lengthy list of websites, and identifies SSL certificates. It was run from an AWS Ubuntu instance.

---
+++ website SSL checker.sh
@@ -0,0 +1,17 @@
+# test if a site exists and if it is running SSL
+# plans for improvements: test if there is a redirect (3xx http code) and pass the redirected URL recursively.
+testSSL() {
+  site=$1
+  site_exists=`curl --silent --head --location --output /dev/null --write-out '%{http_code}' $site`
+  if [ $site_exists != '000' ]; then
+    if true | openssl s_client -connect $site:443 2>/dev/null | openssl x509 -noout -checkend 0; then
+      : #do nothing
+    else
+      echo $site
+    fi
+  fi
+}
+
+run() {
+testSSL google.com
+}
